46acca3119be2c36015b501f4e6a24884910ccba
lesismal <40462947+lesismal@users.noreply.github.com>
2021-03-05 20:50:44 +0800

    - implement response

--- response.go
+++ response.go
@@ -1,7 +1,7 @@
 package nbhttp
 
 import (
-	"net"
+	"io"
 	"net/http"
 )
 
@@ -14,12 +14,40 @@ import (
 // Response represents the server side of an HTTP response.
 // todo:
 type Response struct {
-	conn net.Conn
-	req  *http.Request // request for this response
+	writer    io.Writer
+	processor Processor
 
-	status   int // status code passed to WriteHeader
-	header   http.Header
-	trailers http.Header
+	sequence uint64
+	request  *http.Request // request for this response
 
-	bodyBuffers [][]byte
+	statusCode int // status code passed to WriteHeader
+	status     string
+	header     http.Header
+	trailers   http.Header
+
+	body []byte
+}
+
+// Header .
+func (response *Response) Header() http.Header {
+	return response.header
+}
+
+// Write .
+func (response *Response) Write(data []byte) (int, error) {
+	response.WriteHeader(http.StatusOK)
+	if len(data) > 0 {
+		response.body = append(response.body, data...)
+	}
+	return response.processor.WriteTo(response.writer, data)
+}
+
+// WriteHeader .
+func (response *Response) WriteHeader(statusCode int) {
+	if response.statusCode == 0 {
+		response.status = http.StatusText(statusCode)
+		if response.status != "" {
+			response.statusCode = statusCode
+		}
+	}
 }
